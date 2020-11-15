a6b8ec34d22e5005bf2f90dc2e883565ab5b279f
T4securitys <74420672+T4securitys@users.noreply.github.com>
2020-11-15 11:24:07 +0530

    Update and rename .gitnore to .py

---
+++ .py
@@ -0,0 +1,24 @@
+#password.py
+#<==WELCOME==>
+#Used for this file - termux,github, python 3.1
+#<==License==>
+#Do not fork or copy it or make changes
+#If license not followed that user will be reported
+#<==Warning==>
+#Android version under 3.0 can get hanged
+
+Name = "Github"
+Password = "(1)5-7-9,(2)4-8"
+Password_hider = True
+Hack_protection = True 
+User_secret_code ="10,01,01,11,10,00,01,01,01,01,10"
+
+
+if password_hider = True:
+ print("Password is protected")
+
+if password_hider = False:
+print("Password is (1)5-7-9,(2)4-8,solve this and you will get the password")
+
+#r/use/bin/0/
+
